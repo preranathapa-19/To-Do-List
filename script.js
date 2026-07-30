b9eea4f0a0cc99c09e28a29a6f7e7024ed2c1095
prerana <preranamagar19@gmail.com>
2026-07-30 14:58:12 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// //Seclect Dom Elements
+
+// const input = document.getElementById('todo-input')
+// const addBtn = document.getElementById('add-btn')
+// const list = document.getElementById('todo-list')
+
+// // Try to load saved todas from localstorage (if any)
+// const saved = localStorage.getItem('todas');
+// const todas = saved? JSON.parse(saved) : [];
+
+// function saveTodos(){
+//     //save current todos array to localstorage
+//     localStorage.setItem('todos', JSON.stringify(todas));
+// }
+
+// //create a dom node for a todo object and append it to the list 
+// function 
+
+
+
+
